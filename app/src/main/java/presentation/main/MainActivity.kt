8db046ec487c7977c7c7f94f2d6738a367e4d599
Aysun <aysunagazad@gmail.com>
2026-09-20 20:12:35 +0400

    feat: initial implementation of android recruitment task

---
+++ app/src/main/java/presentation/main/MainActivity.kt
@@ -0,0 +1,21 @@
+package presentation.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sockettest.R
+import com.example.sockettest.databinding.ActivityMainBinding
+import presentation.market.MarketFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, MarketFragment())
+                .commit()
+    }
+}
